c2574759e220cce5b4616611ea28612043b5f4f5
Stefano Garuti <sgaruti@gmail.com>
2020-09-01 16:29:26 +0200

    fix sending the OfflineSubmit event. close issue #1

--- public/js/tawkto-api-client.js
+++ public/js/tawkto-api-client.js
@@ -1,8 +1,8 @@
 var Tawk_API = Tawk_API || {};
 var is_ongoing = 0;
 var startime;
-var ongoing_check_id= setInterval(function(){
 
+var ongoing_check_id= setInterval(function(){
 	if(Tawk_API.isChatOngoing()){
 		if( is_ongoing == 0){
 			is_ongoing = 1;
@@ -13,6 +13,7 @@ var ongoing_check_id= setInterval(function(){
 			nowtime = new Date();
 			(TAE_VAR.debug) ? console.log('Ongoing status nowtime: ', nowtime) : '';
 			diff =  nowtime - startime;
+      (TAE_VAR.debug) ? console.log('Ongoing status difftime: ', diff) : '';
 			if ( diff > 1000 * TAE_VAR.chat_min_lenght){
 				(TAE_VAR.debug) ? console.log('Send event Ongoing chat to Analytics') : '';
 				ga('send', 'event', { eventCategory: TAE_VAR.eventCategory, eventAction: TAE_VAR.eventActionChatOngoing, eventLabel: '1minuto'});
@@ -29,7 +30,6 @@ var ongoing_check_id= setInterval(function(){
 3000);
 
 Tawk_API.onOfflineSubmit = function(data){
-	alert('ste' . TAE_VAR.debug );
 	(TAE_VAR.debug) ? console.log('Send event Chat offline submit to Analytics') : '';
 	ga('send', 'event', { eventCategory: TAE_VAR.eventCategory, eventAction: TAE_VAR.eventActiononOfflineSubmit});
 };
